24ddccf918dc863872ed46cb7f5792a8cd03e54c
Charlie <charlieboyle252@gmail.com>
2024-08-07 15:21:20 -0500

    addition of more projects, a profile picture, additions to contact page, and a background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
 import './App.css'
-import { Link, Outlet }  from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <div>
+      <header>
+        <div id ='icon'>
+      <a href="https://charliejb1.github.io/ThirdPartyWeatherApi/">
+            <img
+              src="https://media.licdn.com/dms/image/D5603AQFrO3w30vYwuQ/profile-displayphoto-shrink_200_200/0/1666760663116?e=1728518400&v=beta&t=16Ro_YaS_oN0rZu_107usyCg6ElVVR6Dzmner5mGSzE"
+              alt="Picture of Charles Boyle in suit and tie"
+            />
+          </a>
+          </div>
+        <h1>Charles Boyle</h1>
+        <nav>
+          
+          <ul>
+            <li><Link to="/">Projects</Link></li>
+            <li><Link to="/about">AboutMe</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+    </div>
 
-<div>
-  <h2>Charles Boyle</h2>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Projects</Link>
-          </li>
-          <li>
-            <Link to="/about">AboutMe</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
-  <Outlet />
-</div>
-
-        )
+  )
 }
 export default App;
 
