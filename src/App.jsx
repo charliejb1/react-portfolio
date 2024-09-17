6ec041cba378fd94db4d5833998576a18b06c5d7
Charlie <charlieboyle252@gmail.com>
2024-09-17 00:33:30 -0500

    fixed profile picture url to linkedIn

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div>
       <header>
         <div id ='icon'>
-      <a href="https://charliejb1.github.io/ThirdPartyWeatherApi/">
+      <a href="https://www.linkedin.com/in/charles-boyle-37a770243/">
             <img
               src="https://media.licdn.com/dms/image/D5603AQFrO3w30vYwuQ/profile-displayphoto-shrink_200_200/0/1666760663116?e=1728518400&v=beta&t=16Ro_YaS_oN0rZu_107usyCg6ElVVR6Dzmner5mGSzE"
               alt="Picture of Charles Boyle in suit and tie"
