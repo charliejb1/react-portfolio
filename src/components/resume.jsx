2169076d42691b7b61ffd59b401e7f07560e2b68
Charlie <charlieboyle252@gmail.com>
2025-05-13 15:15:17 -0500

    resume fixes such as formatting and grammar

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Pdf from './resume-2025.png';
+import Pdf from './resume-2025.pdf';
 // import Header from './components/header.jsx'
 
 // Resume bullet points with install button
@@ -17,34 +17,50 @@ function Resume() {
         <a id="resume-click" href={Pdf}>Click for PDF</a>
         <div id="resume">
           <h3 id="summary-header">Summary</h3>
-          <p id="summary-text">Aspiring entry-level software developer with a passion to enhance my foundational skillset of full-stack development and learning value-driven solutions for client needs. I am a recent graduate of the Northwestern Full Stack Software Development Bootcamp with previous work experience in private investigations, marketing, and sales. I am currently seeking junior positions in both front-end and back-end development.</p>
-          <h3 id="work-experience">Work Experience</h3>
-          <h4 id="job-1">Private Investigator - Hanrahan Investigations Group - Hillside, IL (Nov 2024 - Jan 2025)</h4>
-          <ul id="job-1-list">
-            <li>Gathered evidence through interviews and database research pertaining to asbestos exposure cases.</li>
-            <li>Compiled detailed written reports of investigation findings for clients.</li>
-            <li>Collected intelligence from public records and sources in the field.</li>
-            <li>Conducted surveillance on individuals suspected of claiming false injuries.</li>
-          </ul>
-          <h4 id="job-2">Digital Media Coordinator - RealtyAds - Chicago, IL (Mar 2023 - Aug 2023)</h4>
-          <ul id="job-2-list">
-            <li>Assisted in creation of managed service promotions and email marketing content ideas.</li>
-            <li>Deactivated broken webpages through coordination with Google and LinkedIn representatives.</li>
-            <li>Created social media pages for new clients with necessary company information.</li>
-          </ul>
+          <p id="summary-text">Aspiring entry-level software developer with a passion to enhance my foundational skillset of full-stack development and learn value-driven solutions for client needs. I am a recent graduate of the Northwestern Full Stack Software Development Bootcamp with previous work experience in private investigations, marketing, and sales. I am currently seeking junior positions in both front-end and back-end development.</p>
 
           <h3 id="education">Education</h3>
-          <h4 id="university">The University of Iowa, Iowa City, IA (Aug 2016 - May 2021)</h4>
+          <h4 id="university">The University of Iowa, Iowa City, IA (Aug 2016–May 2021)</h4>
           <ul id="education-list">
             <li>Bachelor of Arts: Communication Studies and Enterprise Leadership</li>
           </ul>
+
+          <h3 id="skills">Skills</h3>
+          <ul id="skills-list">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>React.js</li>
+            <li>Client Communication</li>
+            <li>Report Writing</li>
+            <li>Evidence-Based Analysis</li>
+          </ul>
+
+          <h3 id="work-experience">Work Experience</h3>
+          <h4 id="job-1">Private Investigator, Hanrahan Investigations Group - Hillside, IL (Nov 2024–Jan 2025)</h4>
+          <ul id="job-1-list">
+            <li>Gathered evidence through interviews and database research pertaining to asbestos exposure cases to support client legal action.</li>
+            <li>Compiled detailed written reports of case findings to aid client decision-making.</li>
+            <li>Collected intelligence from public records and sources in the field.</li>
+            <li>Conducted surveillance on individuals suspected of claiming false injuries.</li>
+          </ul>
+          <h4 id="job-2">Digital Media Coordinator, RealtyAds - Chicago, IL (Mar 2023–Aug 2023)</h4>
+          <ul id="job-2-list">
+            <li>Assisted in creation of managed service promotions and email marketing content ideas to promote the company brand.</li>
+            <li>Deactivated broken webpages through coordination with Google and LinkedIn representatives.</li>
+            <li>Created social media pages for new clients with necessary company information to onboard new clients.</li>
+          </ul>
+
           <h3 id="certifications">Certifications</h3>
-          <h4 id="bootcamp">Northwestern Full Stack Software Development Boot Camp (Feb 2024 - Aug 2024) </h4>
+          <h4 id="bootcamp">Northwestern Full Stack Software Development Boot Camp (Feb 2024–Aug 2024) </h4>
           <ul id="certificate-list">
             <li>6 month bootcamp focused on skill development of HTML, CSS, Javascript, Object-oriented programming, API's, Node.js, Express.js, React.js, PostgreSQL, MongoDB, Model View Controller architecture, and Object Relational Mapping</li>
             <li>Assembled applications using the fundamentals of HTML, CSS and Javascript for simple front-end development skill building.</li>
-            <li>Created and developed applications with node.js for writing files to a server and for interaction between a client and a simple database.</li>
+            <li>Created and developed applications with Node.js for writing files to a server and for interaction between a client and a simple database.</li>
           </ul>
+
           <h3 id="portfolio-websites">Portfolio and Websites</h3>
           <ul id="resume-links">
             <li>Portfolio: https://charlesboyle-react-portfolio.netlify.app/</li>
